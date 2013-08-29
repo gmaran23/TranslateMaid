4840a61923dae5a69c5494c82645f08780254a8c
gmaran23_cp <gmaran23@users.noreply.github.com>
2013-08-29 14:07:21 +0000

    Checking in for initial publish. All verification and test complete.
    19.35, Thursday, 29 August 2013, GMT 5.30+.

---
+++ TranslateMaid/source/TranslateMaid/ModalForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TranslateMaidNS
+{
+    partial class ModalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ModalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 540);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ModalForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ModalForm";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Deactivate += new System.EventHandler(this.ModalForm_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ModalForm_FormClosing);
+            this.Load += new System.EventHandler(this.ModalForm_Load);
+            this.Shown += new System.EventHandler(this.ModalForm_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ModalForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ModalForm_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
